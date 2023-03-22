203158645e8f819fe6fcf95b329cf9e571550e7c
Jihoon Og <og@ualberta.ca>
2023-03-22 16:06:05 -0600

    Extract the number from the mask

--- launchers/default.sh
+++ launchers/default.sh
@@ -13,8 +13,9 @@ dt-launchfile-init
 # NOTE: Use `dt-exec COMMAND` to run the main process (blocking process)
 
 # launching app
-dt-exec roslaunch duckiebot_detection duckiebot_detection_node.launch
-dt-exec roslaunch led_emitter led_emitter_node.launch
+# dt-exec roslaunch duckiebot_detection duckiebot_detection_node.launch
+# dt-exec roslaunch led_emitter led_emitter_node.launch
+dt-exec roslaunch number_detection number_detection_node.launch
 
 # ----------------------------------------------------------------------------
 # YOUR CODE ABOVE THIS LINE
--- packages/number_detection/src/number_detection_node.py
+++ packages/number_detection/src/number_detection_node.py
@@ -11,6 +11,9 @@ import rospy
 import time
 import message_filters
 import typing
+import cv2
+import yaml
+from cv_bridge import CvBridge
 
 import torch
 import torch.nn as nn
@@ -22,7 +25,8 @@ from statistics import mode
 from lane_controller import LaneController
 
 import rospkg
-
+from augmented_reality_basics import Augmenter
+from duckietown_utils import load_homography, load_map
 from duckietown.dtros import DTROS, NodeType, TopicType, DTParam, ParamType
 from duckietown_msgs.msg import (
     Pose2DStamped, 
@@ -37,7 +41,7 @@ from duckietown_msgs.msg import (
     )
 from duckietown_msgs.srv import SetCustomLEDPattern
 from std_msgs.msg import Header, Float32, String, Float64MultiArray, Float32MultiArray, Int32
-from sensor_msgs.msg import CompressedImage
+from sensor_msgs.msg import CompressedImage, CameraInfo
 from geometry_msgs.msg import Point32
 
 import rosbag
@@ -95,43 +99,87 @@ class NumberDetectionNode(DTROS):
         else:
             self.veh_name = "csc22945"
 
+        self.model = MLP(28*28, 10)
+    
+        self.rospack = rospkg.RosPack()
+        self.read_params_from_calibration_file()
+        self.camera_info_dict = self.load_intrinsics()
+
+        # extract parameters from camera_info_dict for apriltag detection
+        f_x = self.camera_info_dict['camera_matrix']['data'][0]
+        f_y = self.camera_info_dict['camera_matrix']['data'][4]
+        c_x = self.camera_info_dict['camera_matrix']['data'][2]
+        c_y = self.camera_info_dict['camera_matrix']['data'][5]
+        self.camera_params = [f_x, f_y, c_x, c_y]
+        K_list = self.camera_info_dict['camera_matrix']['data']
+        self.K = np.array(K_list).reshape((3, 3))
+
+        self.augmenter = Augmenter(self.homography, self.camera_info_msg)
+
         # Static parameters
         self.update_freq = 10
         self.rate = rospy.Rate(self.update_freq)
-        self.rospack = rospkg.RosPack()
 
         # Publishers
-        ## Publish commands to the motors
-        self.pub_motor_commands = rospy.Publisher(f'/{self.veh_name}/wheels_driver_node/wheels_cmd', WheelsCmdStamped, queue_size=1)
-        self.pub_car_cmd = rospy.Publisher(f'/{self.veh_name}/car_cmd_switch_node/cmd', Twist2DStamped, queue_size=1, dt_topic_type=TopicType.CONTROL)
-        
-        ## LED emitter service
-        service_name = f'/{self.veh_name}/led_emitter_node/set_custom_pattern'
-        rospy.wait_for_service(service_name)
-        self.LED_emitter_service = rospy.ServiceProxy(service_name, SetCustomLEDPattern, persistent=True)
-
+        self.pub_number_bb = rospy.Publisher(f"/{self.veh_name}/number_detection_node/image/compressed", CompressedImage, queue_size=1)
+        self.pub_cropped_number = rospy.Publisher(f"/{self.veh_name}/number_detection_node/cropped_number/image/compressed", CompressedImage, queue_size=1)
         # Subscribers
         ## Subscribe to the lane_pose node
-        self.sub_lane_reading = rospy.Subscriber(f"/{self.veh_name}/lane_filter_node/lane_pose", LanePose, self.cb_lane_pose, queue_size = 1)
-        self.sub_segment_list = rospy.Subscriber(f"/{self.veh_name}/line_detector_node/segment_list", SegmentList, self.cb_segments, queue_size=1)
-        self.sub_distance_to_robot_ahead = rospy.Subscriber(f"/{self.veh_name}/duckiebot_distance_node/distance", Float32, self.cb_vehicle_distance, queue_size=1)
-        self.sub_centers = rospy.Subscriber(f"/{self.veh_name}/duckiebot_detection_node/centers", VehicleCorners, self.cb_vehicle_centers, queue_size=1)
-        self.sub_circle_pattern_image = rospy.Subscriber(f"/{self.veh_name}/duckiebot_detection_node/detection_image/compressed", CompressedImage, queue_size=1)
-        self.sub_detection = rospy.Subscriber(f"/{self.veh_name}/duckiebot_detection_node/detection", BoolStamped, self.cb_detection, queue_size=1)
-        self.sub_tag_id = rospy.Subscriber(f"/{self.veh_name}/tag_id", Int32, self.cb_tag_id, queue_size=1)
+        self.sub_images = rospy.Subscriber(f"/{self.veh_name}/camera_node/image/compressed", CompressedImage, self.cb_image, queue_size=1)
         
+        self.load_model()
+
         self.log("Initialized")
 
+    def cb_image(self, msg):
+        br = CvBridge()
+        # Convert image to cv2
+        raw_image = br.compressed_imgmsg_to_cv2(msg)
+        # processed_image = self.augmenter.process_image(raw_image)
+
+        rangomax = np.array([255,175,50]) # B,G,R
+        rangomin = np.array([60,60,0])
+        mask = cv2.inRange(raw_image, rangomin, rangomax)
+        # reduce the noise
+        opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, np.ones((5,5),np.uint8), iterations = 2)
+        x,y,w,h = cv2.boundingRect(opening)
+
+        cv2.rectangle(raw_image, (x,y), (x + w, y + h), (0,255,0), 2)
+
+        number = raw_image[y:y+h, x:x+w]
+        black_max = np.array([50,50,50])
+        black_min = np.array([0,0,0])
+        number_mask = cv2.inRange(number, black_min, black_max)
+        # processed_number = cv2.bitwise_and(number, number_mask)
+        self.pub_processed_image(raw_image)
+        self.pub_processed_image(number_mask)
+    def pub_processed_image(self, image):
+        compressed_image = CompressedImage()
+        compressed_image.header.stamp = rospy.Time.now()
+        compressed_image.format = "jpeg"
+        compressed_image.data = np.array(cv2.imencode('.jpg',image)[1]).tostring()
+
+        self.pub_number_bb.publish(compressed_image)
+
+    def read_params_from_calibration_file(self):
+        # Get static parameters
+        file_name_ex = self.get_extrinsic_filepath(self.veh_name)
+        self.homography = self.readYamlFile(file_name_ex)
+        self.camera_info_msg = rospy.wait_for_message(f'/{self.veh_name}/camera_node/camera_info', CameraInfo)
+
+
     def load_model(self):
-        model_file_folder = self.rospack.get_path('number_detection') + '/config/tut1-model.pt'
+        model_file_folder = self.rospack.get_path('number_detection') + '/config/MNIST_model.pt'
+        self.model.load_state_dict(torch.load(model_file_folder, map_location=torch.device('cpu')))
+        self.model.eval()
 
     def load_intrinsics(self):
         # Find the intrinsic calibration parameters
         # cali_file_folder = '/data/config/calibrations/camera_intrinsic/'
-        # self.frame_id = self.veh + '/camera_optical_frame'
-        # self.cali_file = cali_file_folder + self.veh + ".yaml"
+        # self.frame_id = self.veh_name + '/camera_optical_frame'
+        # self.cali_file = cali_file_folder + self.veh_name + ".yaml"
 
-        self.cali_file = self.rospack.get_path('duckiebot_detection') + f"/config/calibrations/camera_intrinsic/{self.veh}.yaml"
+        self.cali_file = self.rospack.get_path('duckiebot_detection') + f"/config/calibrations/camera_intrinsic/{self.veh_name}.yaml"
 
         # Locate calibration yaml file or use the default otherwise
         rospy.loginfo(f'Looking for calibration {self.cali_file}')
@@ -155,6 +203,34 @@ class NumberDetectionNode(DTROS):
 
         cali_file = cali_file_folder + name + ".yaml"
         return cali_file
+
+    def readYamlFile(self,fname):
+        with open(fname, 'r') as in_file:
+            try:
+                yaml_dict = yaml.load(in_file)["homography"]
+                return yaml_dict
+            except yaml.YAMLError as exc:
+                self.log("YAML syntax error. File: %s fname. Exc: %s"
+                         %(fname, exc), type='fatal')
+                rospy.signal_shutdown()
+                return
+
+    def readYamlFile2(self,fname):
+        """
+            Reads the 'fname' yaml file and returns a dictionary with its input.
+
+            You will find the calibration files you need in:
+            `/data/config/calibrations/`
+        """
+        with open(fname, 'r') as in_file:
+            try:
+                yaml_dict = yaml.load(in_file, Loader=yaml.Loader)
+                return yaml_dict
+            except yaml.YAMLError as exc:
+                self.log("YAML syntax error. File: %s fname. Exc: %s"
+                         %(fname, exc), type='fatal')
+                rospy.signal_shutdown('No calibration file found.')
+                return
 
 if __name__ == '__main__':
     node = NumberDetectionNode(node_name='robot_follower_node')
